15884c729ce6352f268b74ea51f1c3543e0f7dea
xEngine-XD <68985963+xEngine-XD@users.noreply.github.com>
2020-08-24 16:29:00 +1000

    test push
    camera + player scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 0.125f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 position = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, position, speed);
+        transform.position = smoothPosition;
+
+        //this one is for more dynamic camera :)
+        //transform.LookAt(target);
+
+    }
+}
