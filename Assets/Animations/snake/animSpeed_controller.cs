4d440c8f075592d068053b0c126556096523ae1c
pGlasby <prudenceglasby@gmail.com>
2020-10-25 12:30:47 +1000

    animationImports

---
+++ Assets/Animations/snake/animSpeed_controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animSpeed_controller : MonoBehaviour
+{
+
+    //public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create velocity variable
+        float velocity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if velocity has changed
+
+
+        //Update velocity variable to hold the current velocity
+        
+    }
+}
